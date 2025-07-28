96b9c10ce6503fc501a8e211cd1a68f147679544
Tanveer singh naruka <ts0310477@gmail.com>
2025-07-28 11:25:34 +0530

    Added project-1 properly

---
+++ project-2/src/components/Button/button.jsx
@@ -0,0 +1,14 @@
+import styles from "./Button.module.css";
+const button = ({ isOutline, icon, text, ...rest }) => {
+  return (
+    <button
+      {...rest}
+      className={isOutline ? styles.outline_btn : styles.primary_btn}
+    >
+      {icon}
+      {text}
+    </button>
+  );
+};
+
+export default button;
---
+++ project-2/src/components/Navigation/Navigation.jsx
@@ -0,0 +1,19 @@
+import styles from "./Navigation.module.css";
+
+const Navigation = () => {
+  return (
+    <nav className={`${styles.navigation} container`}>
+      <div className="logo">
+        <img src="/image/logo.png"  className="img"alt="do some coding logo" />
+      </div>
+
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
